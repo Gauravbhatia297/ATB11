b471bc61ebc08303ac93b1e21bbaf64a1b2a452b
gauravbhatia297 <gauravbhatia297@gmail.com>
2025-03-11 08:29:28 -0700

    commit

--- src/test1/lab5.java
+++ src/test1/lab5.java
@@ -2,8 +2,11 @@ package test1;
 
 public class lab5 {
     public static void main(String[] args) {
+        int a=5;
         System.out.println("Hi this is my program");
         System.out.println("Hi this other added");
+        System.out.printf("the value of a is %d",a);
+
 
     }
 
