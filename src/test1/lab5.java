750e1d4d106ef1f21f4ffe8a8c222fa418907ef8
gauravbhatia297 <gauravbhatia297@gmail.com>
2025-03-11 08:14:35 -0700

    commit

--- src/test1/lab5.java
+++ src/test1/lab5.java
@@ -3,6 +3,8 @@ package test1;
 public class lab5 {
     public static void main(String[] args) {
         System.out.println("Hi this is my program");
+        System.out.println("Hi this other added");
+
     }
 
     }
