70e741a05454486b93a7bb454129a64a44460af8
gauravbhatia297 <gauravbhatia297@gmail.com>
2025-03-12 20:11:04 -0700

    commit

---
+++ src/ternary/T1.java
@@ -0,0 +1,19 @@
+package ternary;
+
+public class T1 {
+
+    public static void main(String[] args) {
+            // Nested Ternary
+            // result = condition1 ? expression1 : (condition2 ? expression2 : expression3);
+
+            int number = 25;
+            String result = (number >10) ? (number < 20 ? "N>20" : "N<20") : "B";
+            System.out.println(result);
+
+
+
+
+
+        }
+    }
+
